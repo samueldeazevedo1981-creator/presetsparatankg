f2c4edbcb5b88b3b13d6a1a1f75b1e5c4b2831bc
samueldeazevedo1981-creator <samueldeazevedo1981@gmail.com>
2026-08-20 12:58:40 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// EDITE APENAS ESTE ARQUIVO PARA INSERIR SEUS LINKS DE CHECKOUT.
+// Exemplo: "https://pay.seusite.com/tankg-basico"
+window.TANKG_CONFIG = {
+  checkoutBasico: "#",
+  checkoutProfissional: "#"
+};
