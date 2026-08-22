8fdc5142053f7a120bf384d38f2c6b1cb301f95a
samueldeazevedo1981-creator <samueldeazevedo1981@gmail.com>
2026-08-22 15:30:58 -0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// EDITE APENAS ESTE ARQUIVO PARA INSERIR SEUS LINKS DE CHECKOUT.
-// Exemplo: "https://pay.seusite.com/tankg-basico"
-window.TANKG_CONFIG = {
-  checkoutBasico: "#",
-  checkoutProfissional: "#"
-};
