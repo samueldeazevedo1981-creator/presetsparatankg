7cd82260f5fda961c577c9169a309cb7211c600d
samueldeazevedo1981-creator <samueldeazevedo1981@gmail.com>
2026-08-25 18:31:08 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// CONFIGURAÇÃO DA LANDING PAGE TANK-G
+// Substitua META_PIXEL_ID pelo ID numérico real do seu Pixel da Meta.
+// externalIdManual pode receber um ID interno seu (CRM/aluno/cliente) quando existir.
+window.TANKG_CONFIG = {
+  checkoutBasico: "https://chk.eduzz.com/40QRD8RQ9B",
+  checkoutProfissional: "https://chk.eduzz.com/E0DKB4KD91",
+  metaPixelId: "META_PIXEL_ID",
+  externalIdManual: ""
+};
